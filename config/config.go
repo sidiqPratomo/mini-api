73c960b378f01369e2c91caddebb5dd4643b0556
sidiqPratomo <pratomosidiq123@gmail.com>
2025-04-11 11:00:56 +0700

    fix env

--- config/config.go
+++ config/config.go
@@ -3,29 +3,36 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("❌ Could not load .env file")
+	}
     host := os.Getenv("DB_HOST")
     portStr := os.Getenv("DB_PORT")
     port, err := strconv.Atoi(portStr)
     if err != nil {
-        return nil, fmt.Errorf("invalid port number: %v", err)
+		return nil, fmt.Errorf("invalid port number: %v", err)
     }
-
-    user := os.Getenv("DB_USER")
+	
+    user := os.Getenv("DB_USERNAME")
     password := os.Getenv("DB_PASSWORD")
     dbname := os.Getenv("DB_DATABASE")
-
+	
     dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
-        user, password, host, port, dbname)
-
+	user, password, host, port, dbname)
+	
+	fmt.Println("DSN :", dsn)
     db, err := sql.Open("mysql", dsn)
     if err != nil {
         return nil, fmt.Errorf("failed to open DB: %v", err)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-sql-driver/mysql v1.9.2
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.37.0
 )
 
